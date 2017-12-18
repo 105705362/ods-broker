820b6fbd1048deb060d4fe315da173b47f096336
Chaos Eternal <chaos@shlug.org>
2017-12-18 20:19:54 +0800

    env

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
 from itertools import chain
 from functools import reduce
 import adapters
-
+import os
+import os.path
+from bosh_api import *
 
 _all_catalogues = [getattr(getattr(adapters, m), 'service_catalogue')
                  for m in adapters.__all__
@@ -17,3 +19,19 @@ _merge = lambda l, r: {k: _merge(r.get(k), l.get(k) )
                        for k in chain(r, l)}
 
 ALL_SERVICE_CATALOGUES = reduce(_merge, _all_catalogues, {})
+
+bosh_env_ip = os.getenv("BOSH_ENVIRONMENT")
+bosh_client = os.getenv("BOSH_CLIENT")
+bosh_client_secret = os.getenv("BOSH_CLIENT_SECRET")
+
+_e_bosh_cacert = os.getenv("BOSH_CACERT")
+if os.path.exists.(_e_bosh_cacert):
+    bosh_cacert = _e_bosh_cacert
+else:
+    import tempfile
+    _f , bosh_cacert = tempfile.mkstemp(".cert", "bosh_ca_", "/tmp/")
+    _fp = os.fdopen(_f, "w")
+    _fp.write(_e_bosh_cacert)
+    _fp.close()
+
+BOSH = BoshEnv(bosh_env_ip, bosh_client, bosh_client_secret, cacert=bosh_cacert)
